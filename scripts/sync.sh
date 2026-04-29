0430acb1c801754f02143bfae2ea24558c4672e8
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2026-04-29 10:40:02 +0700

    BUSETTTT

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,7 +2,7 @@
 
 set -exv
 
-nama_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
+name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
 only_sync=$(grep 'repo sync' $CIRRUS_WORKING_DIR/build.sh)
 a=$(grep 'Cannot remove project' Sync-rom.log -m1|| true)
 b=$(grep "^fatal: remove-project element specifies non-existent project" Sync-rom.log -m1 || true)
